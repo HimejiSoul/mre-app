9e9a504b80bd102ad2563797d901555cf992b7b6
HimejiSoul <perseushilmy@gmail.com>
2024-04-26 14:23:45 +0700

    feat:add edit feature

--- app/dashboard/keluarga-berencana/[id]/edit/page.tsx
+++ app/dashboard/keluarga-berencana/[id]/edit/page.tsx
@@ -3,16 +3,16 @@ import { fetchPatientById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 import EditKBForm from '@/app/ui/keluarga-berencana/edit-kb-form';
 export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id;
-  const patientData = await fetchPatientById(id);
-  console.log(patientData);
+  const id_pasien = params.id;
+  const patientData = await fetchPatientById(id_pasien);
+  // console.log(id_pasien);
   if (!patientData) {
     notFound();
   }
   return (
     <main>
       <SectionTitle>Tambah Pasien Layanan Keluarga Berencana</SectionTitle>
-      <EditKBForm patient={patientData} />
+      <EditKBForm patient={patientData} id_pasien={id_pasien} />
     </main>
   );
 }
