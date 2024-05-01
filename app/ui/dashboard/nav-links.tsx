f6192c442a1245bb754064bfbb76890af275cf51
MTharreq <mtharreq@gmail.com>
2024-05-01 23:33:26 +0700

    feat: add role Bidan and Superadmin to dashboard

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,30 +1,59 @@
 'use client';
 import * as Icon from '../icons';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { redirect, usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { useSession } from 'next-auth/react';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Dashboard', href: '/dashboard', icon: Icon.Home },
+  { name: 'Dashboard', href: '/dashboard', icon: Icon.Home, role: 'both' },
   {
     name: 'Keluarga Berencana',
     href: '/dashboard/keluarga-berencana',
     icon: Icon.KB,
+    role: 'bidan',
   },
   {
     name: 'Periksa Kehamilan',
     href: '/dashboard/periksa-kehamilan',
     icon: Icon.Kehamilan,
+    role: 'bidan',
+  },
+  {
+    name: 'Manajemen Akun',
+    href: '/dashboard/manajemen-akun',
+    icon: Icon.ManajemenAkun,
+    role: 'superadmin',
   },
 ];
 
 export default function NavLinks() {
   const pathname = usePathname();
+
+  // FIXME: Fix this ts error
+  const session: any = useSession();
+  if (!session) {
+    alert('Data error');
+  }
+
+  const role = session?.data?.user.role;
+
+  // Filter links based on role and 'both' role
+  const filteredLinks = links.filter(
+    (link) => link.role === role || link.role === 'both',
+  );
+
+  // Check if the current pathname exists in filtered links
+  const isRedirect = filteredLinks.some((link) => link.href === pathname);
+  if (!isRedirect) {
+    redirect('/dashboard');
+  }
+
   return (
     <div className="space-y-2">
-      {links.map((link, i) => {
+      {filteredLinks.map((link, i) => {
         const LinkIcon = link.icon;
         return (
           <Link
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,4 +1,5 @@
 import { useSession } from 'next-auth/react';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export function Profile() {
   // FIXME: Fix this ts error
@@ -10,8 +11,17 @@ export function Profile() {
 
   return (
     <div className="flex flex-col">
-      <p className="font-bold capitalize">Hello, {data?.username}</p>
-      <p className="text-xs text-blue-600">{data?.id}</p>
+      {data ? (
+        <>
+          <p className="font-bold capitalize">Hello, {data?.full_name}</p>
+          <p className="text-xs capitalize text-blue-600">{data?.role}</p>
+        </>
+      ) : (
+        <div className="flex flex-col items-end gap-2">
+          <Skeleton className="h-4 w-[200px] bg-slate-200" />
+          <Skeleton className="h-2 w-[150px] bg-slate-200" />
+        </div>
+      )}
     </div>
   );
 }
