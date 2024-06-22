62d94cf036b5a676305e4b021f8e2cd795f17ab3
HimejiSoul <perseushilmy@gmail.com>
2024-06-22 14:50:25 +0700

    feat: add toast login

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,9 +1,27 @@
 import { useSession } from 'next-auth/react';
 import { Skeleton } from '@/components/ui/skeleton';
+import { useEffect, useState } from 'react';
+import { toast } from '@/components/ui/use-toast';
 
 export function Profile() {
-  // FIXME: Fix this ts error
   const { data: session, status } = useSession();
+  const [hasWelcomed, setHasWelcomed] = useState(false);
+
+  useEffect(() => {
+    if (status === 'authenticated' && session?.user && !hasWelcomed) {
+      try {
+        toast({
+          title: `Selamat Datang ${session.user.full_name}`,
+        });
+        setHasWelcomed(true);
+      } catch (error) {
+        toast({
+          title: `Error`,
+        });
+      }
+    }
+  }, [status, session, hasWelcomed]);
+
   if (status === 'loading' || !session?.user) {
     return (
       <div className="flex flex-col items-end gap-2">
@@ -12,12 +30,13 @@ export function Profile() {
       </div>
     );
   }
-  const data: any = session.user;
+
+  // const data: any = session.user;
 
   return (
     <div className="flex flex-col">
-      <p className="font-bold capitalize">Hello, {data.full_name}</p>
-      <p className="text-xs capitalize text-blue-600">{data.role}</p>
+      <p className="font-bold capitalize">Hello, {session.user.full_name}</p>
+      <p className="text-xs capitalize text-blue-600">{session.user.role}</p>
     </div>
   );
 }
