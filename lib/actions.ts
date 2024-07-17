3df12155c489726948b6ea717e24408332893894
HimejiSoul <perseushilmy@gmail.com>
2024-07-17 11:39:12 +0700

    fix: add id_pasien to edit and soap

--- lib/actions.ts
+++ lib/actions.ts
@@ -63,6 +63,7 @@ export async function createPatient(formData: FormData, id_layanan: number) {
   try {
     const response = await axios.post(apiEndpoint, data);
     console.log(response.data);
+    return response.data.id;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       const errorMessage =
